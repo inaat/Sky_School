46f7c497a3c8c110d7632390b32724642588b7e1
inaat <inayatullahkks@gmail.com>
2021-09-05 15:51:47 +0500

    database file

---
+++ sky_school.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 05, 2021 at 12:43 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sky_school`
+--
+
+--
+-- Dumping data for table `currencies`
+--
+
+INSERT INTO `currencies` (`id`, `country`, `currency`, `code`, `symbol`, `thousand_separator`, `decimal_separator`, `created_at`, `updated_at`) VALUES
+(1, 'Albania', 'Leke', 'ALL', 'Lek', ',', '.', NULL, NULL),
+(2, 'America', 'Dollars', 'USD', '$', ',', '.', NULL, NULL),
+(3, 'Afghanistan', 'Afghanis', 'AF', '؋', ',', '.', NULL, NULL),
+(4, 'Argentina', 'Pesos', 'ARS', '$', ',', '.', NULL, NULL),
+(5, 'Aruba', 'Guilders', 'AWG', 'ƒ', ',', '.', NULL, NULL),
+(6, 'Australia', 'Dollars', 'AUD', '$', ',', '.', NULL, NULL),
+(7, 'Azerbaijan', 'New Manats', 'AZ', 'ман', ',', '.', NULL, NULL),
+(8, 'Bahamas', 'Dollars', 'BSD', '$', ',', '.', NULL, NULL),
+(9, 'Barbados', 'Dollars', 'BBD', '$', ',', '.', NULL, NULL),
+(10, 'Belarus', 'Rubles', 'BYR', 'p.', ',', '.', NULL, NULL),
+(11, 'Belgium', 'Euro', 'EUR', '€', ',', '.', NULL, NULL),
+(12, 'Beliz', 'Dollars', 'BZD', 'BZ$', ',', '.', NULL, NULL),
+(13, 'Bermuda', 'Dollars', 'BMD', '$', ',', '.', NULL, NULL),
+(14, 'Bolivia', 'Bolivianos', 'BOB', '$b', ',', '.', NULL, NULL),
+(15, 'Bosnia and Herzegovina', 'Convertible Marka', 'BAM', 'KM', ',', '.', NULL, NULL),
+(16, 'Botswana', 'Pula\'s', 'BWP', 'P', ',', '.', NULL, NULL),
+(17, 'Bulgaria', 'Leva', 'BG', 'лв', ',', '.', NULL, NULL),
+(18, 'Brazil', 'Reais', 'BRL', 'R$', ',', '.', NULL, NULL),
+(19, 'Britain [United Kingdom]', 'Pounds', 'GBP', '£', ',', '.', NULL, NULL),
+(20, 'Brunei Darussalam', 'Dollars', 'BND', '$', ',', '.', NULL, NULL),
+(21, 'Cambodia', 'Riels', 'KHR', '៛', ',', '.', NULL, NULL),
+(22, 'Canada', 'Dollars', 'CAD', '$', ',', '.', NULL, NULL),
+(23, 'Cayman Islands', 'Dollars', 'KYD', '$', ',', '.', NULL, NULL),
+(24, 'Chile', 'Pesos', 'CLP', '$', ',', '.', NULL, NULL),
+(25, 'China', 'Yuan Renminbi', 'CNY', '¥', ',', '.', NULL, NULL),
+(26, 'Colombia', 'Pesos', 'COP', '$', ',', '.', NULL, NULL),
+(27, 'Costa Rica', 'Colón', 'CRC', '₡', ',', '.', NULL, NULL),
+(28, 'Croatia', 'Kuna', 'HRK', 'kn', ',', '.', NULL, NULL),
+(29, 'Cuba', 'Pesos', 'CUP', '₱', ',', '.', NULL, NULL),
+(30, 'Cyprus', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(31, 'Czech Republic', 'Koruny', 'CZK', 'Kč', ',', '.', NULL, NULL),
+(32, 'Denmark', 'Kroner', 'DKK', 'kr', ',', '.', NULL, NULL),
+(33, 'Dominican Republic', 'Pesos', 'DOP ', 'RD$', ',', '.', NULL, NULL),
+(34, 'East Caribbean', 'Dollars', 'XCD', '$', ',', '.', NULL, NULL),
+(35, 'Egypt', 'Pounds', 'EGP', '£', ',', '.', NULL, NULL),
+(36, 'El Salvador', 'Colones', 'SVC', '$', ',', '.', NULL, NULL),
+(37, 'England [United Kingdom]', 'Pounds', 'GBP', '£', ',', '.', NULL, NULL),
+(38, 'Euro', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(39, 'Falkland Islands', 'Pounds', 'FKP', '£', ',', '.', NULL, NULL),
+(40, 'Fiji', 'Dollars', 'FJD', '$', ',', '.', NULL, NULL),
+(41, 'France', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(42, 'Ghana', 'Cedis', 'GHS', '¢', ',', '.', NULL, NULL),
+(43, 'Gibraltar', 'Pounds', 'GIP', '£', ',', '.', NULL, NULL),
+(44, 'Greece', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(45, 'Guatemala', 'Quetzales', 'GTQ', 'Q', ',', '.', NULL, NULL),
+(46, 'Guernsey', 'Pounds', 'GGP', '£', ',', '.', NULL, NULL),
+(47, 'Guyana', 'Dollars', 'GYD', '$', ',', '.', NULL, NULL),
+(48, 'Holland [Netherlands]', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(49, 'Honduras', 'Lempiras', 'HNL', 'L', ',', '.', NULL, NULL),
+(50, 'Hong Kong', 'Dollars', 'HKD', '$', ',', '.', NULL, NULL),
+(51, 'Hungary', 'Forint', 'HUF', 'Ft', ',', '.', NULL, NULL),
+(52, 'Iceland', 'Kronur', 'ISK', 'kr', ',', '.', NULL, NULL),
+(53, 'India', 'Rupees', 'INR', '₹', ',', '.', NULL, NULL),
+(54, 'Indonesia', 'Rupiahs', 'IDR', 'Rp', ',', '.', NULL, NULL),
+(55, 'Iran', 'Rials', 'IRR', '﷼', ',', '.', NULL, NULL),
+(56, 'Ireland', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(57, 'Isle of Man', 'Pounds', 'IMP', '£', ',', '.', NULL, NULL),
+(58, 'Israel', 'New Shekels', 'ILS', '₪', ',', '.', NULL, NULL),
+(59, 'Italy', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(60, 'Jamaica', 'Dollars', 'JMD', 'J$', ',', '.', NULL, NULL),
+(61, 'Japan', 'Yen', 'JPY', '¥', ',', '.', NULL, NULL),
+(62, 'Jersey', 'Pounds', 'JEP', '£', ',', '.', NULL, NULL),
+(63, 'Kazakhstan', 'Tenge', 'KZT', 'лв', ',', '.', NULL, NULL),
+(64, 'Korea [North]', 'Won', 'KPW', '₩', ',', '.', NULL, NULL),
+(65, 'Korea [South]', 'Won', 'KRW', '₩', ',', '.', NULL, NULL),
+(66, 'Kyrgyzstan', 'Soms', 'KGS', 'лв', ',', '.', NULL, NULL),
+(67, 'Laos', 'Kips', 'LAK', '₭', ',', '.', NULL, NULL),
+(68, 'Latvia', 'Lati', 'LVL', 'Ls', ',', '.', NULL, NULL),
+(69, 'Lebanon', 'Pounds', 'LBP', '£', ',', '.', NULL, NULL),
+(70, 'Liberia', 'Dollars', 'LRD', '$', ',', '.', NULL, NULL),
+(71, 'Liechtenstein', 'Switzerland Francs', 'CHF', 'CHF', ',', '.', NULL, NULL),
+(72, 'Lithuania', 'Litai', 'LTL', 'Lt', ',', '.', NULL, NULL),
+(73, 'Luxembourg', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(74, 'Macedonia', 'Denars', 'MKD', 'ден', ',', '.', NULL, NULL),
+(75, 'Malaysia', 'Ringgits', 'MYR', 'RM', ',', '.', NULL, NULL),
+(76, 'Malta', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(77, 'Mauritius', 'Rupees', 'MUR', '₨', ',', '.', NULL, NULL),
+(78, 'Mexico', 'Pesos', 'MXN', '$', ',', '.', NULL, NULL),
+(79, 'Mongolia', 'Tugriks', 'MNT', '₮', ',', '.', NULL, NULL),
+(80, 'Mozambique', 'Meticais', 'MZ', 'MT', ',', '.', NULL, NULL),
+(81, 'Namibia', 'Dollars', 'NAD', '$', ',', '.', NULL, NULL),
+(82, 'Nepal', 'Rupees', 'NPR', '₨', ',', '.', NULL, NULL),
+(83, 'Netherlands Antilles', 'Guilders', 'ANG', 'ƒ', ',', '.', NULL, NULL),
+(84, 'Netherlands', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(85, 'New Zealand', 'Dollars', 'NZD', '$', ',', '.', NULL, NULL),
+(86, 'Nicaragua', 'Cordobas', 'NIO', 'C$', ',', '.', NULL, NULL),
+(87, 'Nigeria', 'Nairas', 'NGN', '₦', ',', '.', NULL, NULL),
+(88, 'North Korea', 'Won', 'KPW', '₩', ',', '.', NULL, NULL),
+(89, 'Norway', 'Krone', 'NOK', 'kr', ',', '.', NULL, NULL),
+(90, 'Oman', 'Rials', 'OMR', '﷼', ',', '.', NULL, NULL),
+(91, 'Pakistan', 'Rupees', 'PKR', '₨', ',', '.', NULL, NULL),
+(92, 'Panama', 'Balboa', 'PAB', 'B/.', ',', '.', NULL, NULL),
+(93, 'Paraguay', 'Guarani', 'PYG', 'Gs', ',', '.', NULL, NULL),
+(94, 'Peru', 'Nuevos Soles', 'PE', 'S/.', ',', '.', NULL, NULL),
+(95, 'Philippines', 'Pesos', 'PHP', 'Php', ',', '.', NULL, NULL),
+(96, 'Poland', 'Zlotych', 'PL', 'zł', ',', '.', NULL, NULL),
+(97, 'Qatar', 'Rials', 'QAR', '﷼', ',', '.', NULL, NULL),
+(98, 'Romania', 'New Lei', 'RO', 'lei', ',', '.', NULL, NULL),
+(99, 'Russia', 'Rubles', 'RUB', 'руб', ',', '.', NULL, NULL),
+(100, 'Saint Helena', 'Pounds', 'SHP', '£', ',', '.', NULL, NULL),
+(101, 'Saudi Arabia', 'Riyals', 'SAR', '﷼', ',', '.', NULL, NULL),
+(102, 'Serbia', 'Dinars', 'RSD', 'Дин.', ',', '.', NULL, NULL),
+(103, 'Seychelles', 'Rupees', 'SCR', '₨', ',', '.', NULL, NULL),
+(104, 'Singapore', 'Dollars', 'SGD', '$', ',', '.', NULL, NULL),
+(105, 'Slovenia', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(106, 'Solomon Islands', 'Dollars', 'SBD', '$', ',', '.', NULL, NULL),
+(107, 'Somalia', 'Shillings', 'SOS', 'S', ',', '.', NULL, NULL),
+(108, 'South Africa', 'Rand', 'ZAR', 'R', ',', '.', NULL, NULL),
+(109, 'South Korea', 'Won', 'KRW', '₩', ',', '.', NULL, NULL),
+(110, 'Spain', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(111, 'Sri Lanka', 'Rupees', 'LKR', '₨', ',', '.', NULL, NULL),
+(112, 'Sweden', 'Kronor', 'SEK', 'kr', ',', '.', NULL, NULL),
+(113, 'Switzerland', 'Francs', 'CHF', 'CHF', ',', '.', NULL, NULL),
+(114, 'Suriname', 'Dollars', 'SRD', '$', ',', '.', NULL, NULL),
+(115, 'Syria', 'Pounds', 'SYP', '£', ',', '.', NULL, NULL),
+(116, 'Taiwan', 'New Dollars', 'TWD', 'NT$', ',', '.', NULL, NULL),
+(117, 'Thailand', 'Baht', 'THB', '฿', ',', '.', NULL, NULL),
+(118, 'Trinidad and Tobago', 'Dollars', 'TTD', 'TT$', ',', '.', NULL, NULL),
+(119, 'Turkey', 'Lira', 'TRY', 'TL', ',', '.', NULL, NULL),
+(120, 'Turkey', 'Liras', 'TRL', '£', ',', '.', NULL, NULL),
+(121, 'Tuvalu', 'Dollars', 'TVD', '$', ',', '.', NULL, NULL),
+(122, 'Ukraine', 'Hryvnia', 'UAH', '₴', ',', '.', NULL, NULL),
+(123, 'United Kingdom', 'Pounds', 'GBP', '£', ',', '.', NULL, NULL),
+(124, 'United States of America', 'Dollars', 'USD', '$', ',', '.', NULL, NULL),
+(125, 'Uruguay', 'Pesos', 'UYU', '$U', ',', '.', NULL, NULL),
+(126, 'Uzbekistan', 'Sums', 'UZS', 'лв', ',', '.', NULL, NULL),
+(127, 'Vatican City', 'Euro', 'EUR', '€', '.', ',', NULL, NULL),
+(128, 'Venezuela', 'Bolivares Fuertes', 'VEF', 'Bs', ',', '.', NULL, NULL),
+(129, 'Vietnam', 'Dong', 'VND', '₫', ',', '.', NULL, NULL),
+(130, 'Yemen', 'Rials', 'YER', '﷼', ',', '.', NULL, NULL),
+(131, 'Zimbabwe', 'Zimbabwe Dollars', 'ZWD', 'Z$', ',', '.', NULL, NULL),
+(132, 'Iraq', 'Iraqi dinar', 'IQD', 'د.ع', ',', '.', NULL, NULL),
+(133, 'Kenya', 'Kenyan shilling', 'KES', 'KSh', ',', '.', NULL, NULL),
+(134, 'Bangladesh', 'Taka', 'BDT', '৳', ',', '.', NULL, NULL),
+(135, 'Algerie', 'Algerian dinar', 'DZD', 'د.ج', ' ', '.', NULL, NULL),
+(136, 'United Arab Emirates', 'United Arab Emirates dirham', 'AED', 'د.إ', ',', '.', NULL, NULL),
+(137, 'Uganda', 'Uganda shillings', 'UGX', 'USh', ',', '.', NULL, NULL),
+(138, 'Tanzania', 'Tanzanian shilling', 'TZS', 'TSh', ',', '.', NULL, NULL),
+(139, 'Angola', 'Kwanza', 'AOA', 'Kz', ',', '.', NULL, NULL),
+(140, 'Kuwait', 'Kuwaiti dinar', 'KWD', 'KD', ',', '.', NULL, NULL),
+(141, 'Bahrain', 'Bahraini dinar', 'BHD', 'BD', ',', '.', NULL, NULL);
+
+--
+-- Dumping data for table `general_settings`
+--
+
+INSERT INTO `general_settings` (`id`, `school_name`, `salogan`, `address`, `email`, `website`, `reg_no`, `country`, `state`, `city`, `zip_code`, `mobile`, `alternate_number`, `currency_id`, `start_date`, `owner_id`, `time_zone`, `theme_color`, `session_start_month`, `logo`, `favicon`, `enable_tooltip`, `transaction_edit_days`, `keyboard_shortcuts`, `currency_symbol_placement`, `ref_no_prefixes`, `date_format`, `time_format`, `email_settings`, `sms_settings`, `facebook`, `twitter`, `linkedIn`, `youtube`, `googlePlus`, `instagram`, `whatsApp`, `skype`, `pinterest`, `wordpress`, `created_at`, `updated_at`) VALUES
+(1, 'School Name', 'Known For Perf', 'sdf', 'sdf@gmial.com', NULL, NULL, 'PAkistan', 'swat', 'k.khela', '946', '651320', '561320', 91, '2021-09-04', 1, 'Asia/Karachi', NULL, 1, NULL, NULL, 1, 30, NULL, 'before', NULL, 'm/d/Y', '24', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2016_06_01_000001_create_oauth_auth_codes_table', 1),
+(4, '2016_06_01_000002_create_oauth_access_tokens_table', 1),
+(5, '2016_06_01_000003_create_oauth_refresh_tokens_table', 1),
+(6, '2016_06_01_000004_create_oauth_clients_table', 1),
+(7, '2016_06_01_000005_create_oauth_personal_access_clients_table', 1),
+(8, '2019_08_19_000000_create_failed_jobs_table', 1),
+(9, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(10, '2021_09_02_153640_create_permission_tables', 1),
+(11, '2021_09_02_154543_create_currencies_table', 2),
+(12, '2021_09_02_154727_create_sessions_table', 2),
+(14, '2021_09_03_110504_create_general_settings_table', 3);
+
+--
+-- Dumping data for table `oauth_clients`
+--
+
+INSERT INTO `oauth_clients` (`id`, `user_id`, `name`, `secret`, `provider`, `redirect`, `personal_access_client`, `password_client`, `revoked`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Laravel Personal Access Client', 'ECr1f4FtpZBZmJChmQ4DK69Ew199PHs1YzhUawzl', NULL, 'http://localhost', 1, 0, 0, '2021-09-02 10:44:08', '2021-09-02 10:44:08'),
+(2, NULL, 'Laravel Password Grant Client', 'XqwXcTNzjDs6pEOYcWo9BPQWbhBQhrz7Jgf1kRA0', 'users', 'http://localhost', 0, 1, 0, '2021-09-02 10:44:08', '2021-09-02 10:44:08');
+
+--
+-- Dumping data for table `oauth_personal_access_clients`
+--
+
+INSERT INTO `oauth_personal_access_clients` (`id`, `client_id`, `created_at`, `updated_at`) VALUES
+(1, 1, '2021-09-02 10:44:08', '2021-09-02 10:44:08');
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'session.view', 'web', '2021-09-02 13:46:27', NULL),
+(2, 'session.create', 'web', '2021-09-02 13:46:27', NULL),
+(3, 'session.update', 'web', '2021-09-02 13:46:27', NULL),
+(4, 'session.delete', 'web', '2021-09-02 13:46:27', NULL);
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'web', '2020-03-10 07:39:23', '2020-03-10 07:39:23');
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `session`, `is_active`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(1, '552', 0, NULL, '2021-09-02 15:05:46', '2021-09-02 15:24:30');
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'inayat', 'admin@test.com', NULL, '$2y$10$gFI3kHBvo.9XoFrIvHiBTeOme2cVWvTzojdmF6qjUK5ByhBArSx6.', NULL, '2021-09-02 10:50:57', '2021-09-02 10:50:57');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
